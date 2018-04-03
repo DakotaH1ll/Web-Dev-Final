bea18073c6430c6bb00b5fbda275e09692a644d1
Dakota Hill <dakota.hill@uoit.net>
2018-04-03 14:02:30 -0400

    login.js launches from node now

--- login.js
+++ login.js
@@ -20,16 +20,36 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-  $(document).ready(function(){
-      $('#logInButton').on('click',logIn)
+//  $(document).ready(function(){
+//      $('#logInButton').on('click',logIn)
+//  });
 
-  });
+  // configure sessions
+  app.use(session({
+    genid: function(request) {
+      return uuid();
+    },
+    resave: false,
+    saveUninitialized: false,
+    //cookie: {secure: true},
+    secret: 'apollo slackware prepositional expectations'
+  }));
 
+//function logIn() {
 
-function logIn() {
-
-  if ( $("#studentID").val() == "100523538" && $("#password").val() == "110295"){
-  console.log("Login Successful");
+  //if ( $("#studentID").val() == "100523538" && $("#password").val() == "110295"){
+  //console.log("Login Successful");
   //window.location = "success.html"; // Redirecting to other page.
-  }
-}
+  //}
+//}
+
+app.get('/', function(request, response){
+    response.sendfile('login.html');
+});
+
+app.listen(3001, function() {
+  console.log('Listening on port 3001');
+
+
+
+});
