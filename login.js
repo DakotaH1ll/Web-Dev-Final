ed9d9d92efb8c7fb172aade3ad183274f104cfd0
Dakota Hill <dakota.hill@uoit.net>
2018-04-03 13:53:46 -0400

    Added more modules

--- login.js
+++ login.js
@@ -1,3 +1,23 @@
+var express = require('express');
+var app = express();
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var uuid = require('uuid/v1');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var assert = require('assert');
+
+
+// database config
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/university');
+                 //,{useMongoClient: true});
+
+// middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
   $(document).ready(function(){
