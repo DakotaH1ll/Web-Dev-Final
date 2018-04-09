cef0bd6fc1e797a8b61d26ef13c260ac6d8e8f8b
Dakota Hill <dakota.hill@uoit.net>
2018-04-09 19:23:38 -0400

    jquery support

--- Bad/class.js
+++ Bad/class.js
@@ -11,16 +11,16 @@ function calculate() {
 
   var total = 0;
   //for (i = 0; i < rows; i++){
-  total += (document.getElementById('11').value * 0.01)*(document.getElementById('12').value * 0.01);
-  total += (document.getElementById('21').value * 0.01)*(document.getElementById('22').value * 0.01);
-  total += (document.getElementById('31').value * 0.01)*(document.getElementById('32').value * 0.01);
-  total += (document.getElementById('41').value * 0.01)*(document.getElementById('42').value * 0.01);
-  total += (document.getElementById('51').value * 0.01)*(document.getElementById('52').value * 0.01);
-  total += (document.getElementById('61').value * 0.01)*(document.getElementById('62').value * 0.01);
-  total += (document.getElementById('71').value * 0.01)*(document.getElementById('72').value * 0.01);
-  total += (document.getElementById('81').value * 0.01)*(document.getElementById('82').value * 0.01);
-  total += (document.getElementById('91').value * 0.01)*(document.getElementById('92').value * 0.01);
-  total += (document.getElementById('101').value * 0.01)*(document.getElementById('102').value * 0.01);
+  total += ($('#11').val() * 0.01)*($('#12').val() * 0.01);
+  total += ($('#21').val() * 0.01)*($('#22').val() * 0.01);
+  total += ($('#31').val() * 0.01)*($('#32').val() * 0.01);
+  total += ($('#41').val() * 0.01)*($('#42').val() * 0.01);
+  total += ($('#51').val() * 0.01)*($('#52').val() * 0.01);
+  total += ($('#61').val() * 0.01)*($('#62').val() * 0.01);
+  total += ($('#71').val() * 0.01)*($('#72').val() * 0.01);
+  total += ($('#81').val() * 0.01)*($('#82').val() * 0.01);
+  total += ($('#91').val() * 0.01)*($('#92').val() * 0.01);
+  total += ($('#101').val() * 0.01)*($('#102').val() * 0.01);
 
 currGrade = document.getElementById('currgrade');
 currGrade.innerHTML = ("<b>Your Current Grade Is: " + total*100 + "%</b>");
