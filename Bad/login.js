d38f859ccb98c493ec5987cfc2033f06f858a5ba
Faizan <muhammad.ali9@uoit.net>
2018-04-10 23:49:13 -0400

    fixed file name

--- Bad/login.js
+++ Bad/login.js
@@ -1,75 +1,71 @@
-var showMap = $('#map-canvas');
+var express = require('express');
+var app = express();
 
-  $(document).ready(function(){
-    downloadMap();
-    downloadWeather();
-
-      $('#logInButton').on('click',logIn)
-  });
-
-
-function logIn() {
-
-  if ( $("#studentID").val() == "100523538" && $("#password").val() == "100523538"){
-  window.location = "classes.html"; // Redirecting to other page.
-} else {
-  alert("Incorrect Password Or Username, Please Try Again")
-}
-}
-
-function downloadMap(){
-  var mapOptions = {
-    center: { lat: 43.9459, lng: -78.8967},
-    zoom: 8
-};
-var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-}
-
-function downloadWeather(){
-  var latitude = $('#lat').val();
-  var longitude = $('#lon').val();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var uuid = require('uuid/v1');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var assert = require('assert');
 
 
-  var url = 'http://api.apixu.com/v1/forecast.json?key=0c4d4e086108490d8b733747180603&q=43.9459,-78.8967+&days=7'
+// database config
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/university');
+                 //,{useMongoClient: true});
 
-      $.getJSON(url, function(data) {
-        var current = data.current.temp_c;
-        var high = data.forecast.forecastday[0].day.maxtemp_c;
-        var low = data.forecast.forecastday[0].day.mintemp_c;
-        var feelsLike = data.current.feelslike_c;
-        var condition = data.current.condition.text;
-        var cloudCover = data.current.cloud;
-        var humidity = data.current.humidity;
-        var pressure = data.current.pressure_mb;
-        var direction = data.current.wind_dir;
-        var speed = data.current.wind_kph;
+// middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
+//  $(document).ready(function(){
+//      $('#logInButton').on('click',logIn)
+//  });
 
-        $('#weather').css("background-color", "lightBlue");
-        $('#weather').css("border-color", "black");
-        $('#weather').css("border-width", "2px");
-        $('#weather').css("border-style", "solid");
-        $('#weather').css("list-style-type", "none");
+  // configure sessions
+  app.use(session({
+    genid: function(request) {
+      return uuid();
+    },
+    resave: false,
+    saveUninitialized: false,
+    //cookie: {secure: true},
+    secret: 'apollo slackware prepositional expectations'
+  }));
 
-        $('#weather').append('<p><b>Temperature</b></p>')
-        $('#weather').append('<li>Current: ' + current + '&deg;C</li>')
-        $('#weather').append('<li>High: ' + high + '&deg;C</li>')
-        $('#weather').append('<li>Low: ' + low + '&deg;C</li>')
 
-        $('#weather').append('<li>Feels Like: ' + feelsLike + '&deg;C</li>')
-        $('#weather').append('<p></p>')
-        $('#weather').append('<p><b>Condition</b></p>')
-        $('#weather').append('<li>' + condition + '</li>')
-        $('#weather').append('<li>Cloud Cover: ' + cloudCover + '%</li>')
-        $('#weather').append('<li>Humidity: ' + humidity + '%</li>')
-        $('#weather').append('<li>Pressure: ' + pressure + ' mB</li>')
-        $('#weather').append('<p></p>')
-        $('#weather').append('<p><b>Wind</b></p>')
-        $('#weather').append('<li>Direction: ' + direction + '</li>')
-        $('#weather').append('<li>Speed: ' + speed + ' km/h</li>')
+  // database schema
+  var Schema = mongoose.Schema;
+  var studentSchema = new Schema({
+    sid: {type: String,
+          validate: [/^1[0-9]{8}$/, 'must be 9 digits'],
+          unique: true,
+          index: true},
+          hashedPassword: String
+}, {collection: 'students'});
+  var Student = mongoose.model('student', studentSchema);
 
-      });
 
-}
+
+
+
+//function logIn() {
+
+  //if ( $("#studentID").val() == "100523538" && $("#password").val() == "110295"){
+  //console.log("Login Successful");
+  //window.location = "success.html"; // Redirecting to other page.
+  //}
+//}
+
+app.get('/', function(request, response){
+    response.sendfile('login.html');
+});
+
+app.listen(3001, function() {
+  console.log('Listening on port 3001');
+
+
+
+});
